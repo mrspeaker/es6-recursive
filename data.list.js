de64719d1a597f1081689dac646cd4de7f110b12
Mr Speaker <mrspeaker@gmail.com>
2015-02-14 18:20:42 -0500

    ff added array decomp

--- data.list.js
+++ data.list.js
@@ -1,31 +1,30 @@
 // Helpers
-const xxs = (a) => [a[0], a.slice(1)], // Should be destructured - not impl. in Firefox yet
-	xsx = (a) => [a.slice(0, -1), a[a.length - 1]],
-	isEmpty = (a) => a.length === 0,
+const isEmpty = a => a.length === 0,
 	isEmpties = (...a) => any(isEmpty, a);
 
 // HOF
-const compose = (f, g) => (x) => f(g(x));
+const compose = (f, g) => x => f(g(x));
 
-const flip = (f) => (a, b) => f(b, a);
+const flip = f => (a, b) => f(b, a);
 
 // Collections
 const map = (f, a) => {
 	if (isEmpty(a)) return [];
-	const [x, xs] = xxs(a);
+	const [x, ...xs] = a;
 
 	return [f(x), ...map(f, xs)];
 }
 
 const foldl = (f, a, b) => {
 	if (isEmpty(b)) return a;
-	const [x, xs] = xxs(b);
+	const [x, ...xs] = b;
 
 	return foldl(f, f(a, x), xs);
 }
 
 const foldr = (f, b, a) => {
 	if (isEmpty(a)) return b;
+	const xsx = a => [a.slice(0, -1), a[a.length - 1]]; // Erm;
 	const [xs, x] = xsx(a);
 
 	return foldr(f, f(x, b), xs);
@@ -33,7 +32,7 @@ const foldr = (f, b, a) => {
 
 const filter1 = (f, a) => {
 	if (isEmpty(a)) return [];
-	const [x, xs] = xxs(a);
+	const [x, ...xs] = a;
 
 	return f(x) ? [x, ...filter(f, xs)] : filter(f, xs);
 }
@@ -45,14 +44,14 @@ const filter = (f, a) => foldl((acc, x) => {
 
 const any = (f, a) => {
 	if (isEmpty(a)) return false;
-	const [x, xs] = xxs(a);
+	const [x, ...xs] = a;
 
 	return f(x) ? true : any(f, xs);
 }
 
 const all = (f, a) => {
 	if (isEmpty(a)) return false;
-	const [x, xs] = xxs(a);
+	const [x, ...xs] = a;
 
 	if (!f(x)) return false;
 	return isEmpty(xs) ? true : all(f, xs);
@@ -60,7 +59,7 @@ const all = (f, a) => {
 
 const reverse1 = (a) => {
 	if (isEmpty(a)) return [];
-	const [x, xs] = xxs(a);
+	const [x, ...xs] = a;
 
 	return [...reverse(xs), ...[x]];
 }
@@ -73,7 +72,7 @@ const reverse = (a) => foldr((x, acc) => {
 const elem = (a, b) => {
 	if (isEmpty(b)) return false;
 
-	const [x, xs] = xxs(b);
+	const [x, ...xs] = b;
 	return x === a ? true : elem(a, xs);
 }
 
@@ -81,30 +80,30 @@ const elem1 = (a, b) => foldl((x, acc) => acc || x === a, false, b);
 
 const take = (num, a) => {
 	if (num === 0 || isEmpty(a)) return [];
-	const [x, xs] = xxs(a);
+	const [x, ...xs] = a;
 
 	return [x, ...take(--num, xs)];
 }
 
 const takeWhile = (f, a) => {
 	if (isEmpty(a)) return [];
-	const [x, xs] = xxs(a);
+	const [x, ...xs] = a;
 
 	return f(x) ? [x, ...takeWhile(f, xs)] : [];
 }
 
 const zip = (a, b) => {
 	if (isEmpties(a, b)) return [];
-	const [x, xs] = xxs(a),
-		[y, ys] = xxs(b);
+	const [x, ...xs] = a,
+		[y, ...ys] = b;
 
 	return [[x, y], ...zip(xs, ys)];
 }
 
 const zipWith = (f, a, b) => {
 	if (isEmpties(a, b)) return [];
-	const [x, xs] = xxs(a),
-		[y, ys] = xxs(b);
+	const [x, ...xs] = a,
+		[y, ...ys] = b;
 
 	return [f(x, y), ...zipWith(f, xs, ys)];
 }
@@ -112,8 +111,8 @@ const zipWith = (f, a, b) => {
 const interleave = (a, b) => {
 	if (isEmpty(a)) return b;
 	if (isEmpty(b)) return a;
-	const [x, xs] = xxs(a);
-	const [y, ys] = xxs(b);
+	const [x, ...xs] = a;
+	const [y, ...ys] = b;
 
 	return [x, y, ...interleave(xs, ys)];
 }
@@ -124,7 +123,7 @@ const even = (a) => filter((x) => x % 2 === 0, a);
 const quicksort = (a) => {
 	if (isEmpty(a)) return [];
 
-	const [x, xs] = xxs(a),
+	const [x, ...xs] = a,
 		larger = filter((a) => a >= x, xs),
 		smaller = filter((a) => a < x, xs);
 
